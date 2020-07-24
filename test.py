4123ab4b021243ba6e8dcc56a0be9e4f10b8b232
Thibaut Etienne <thibaut@etienne.pw>
2020-07-24 08:50:03 +0200

    Update test file

--- test.py
+++ test.py
@@ -3,39 +3,34 @@ import time
 
 from tahoma_api.client import TahomaClient
 
-# TODO use .env file
 USERNAME = ""
 PASSWORD = ""
 
 
 async def main() -> None:
-    client = TahomaClient(USERNAME, PASSWORD)
+    async with TahomaClient(USERNAME, PASSWORD) as client:
+        try:
+            await client.login()
+        except Exception as exception:  # pylint: disable=broad-except
+            print(exception)
+            return await client.close()
 
-    try:
-        await client.login()
-    except Exception as exception:  # pylint: disable=broad-except
-        print(exception)
-        return await client.close()
+        devices = await client.get_devices()
 
-    devices = await client.get_devices()
+        for device in devices:
+            print(f"{device.label} ({device.id}) - {device.controllable_name}")
+            print(f"{device.widget} - {device.ui_class}")
+            # print(device.states)
+            # print(device.definition)
 
-    for device in devices:
-        print(f"{device.label} ({device.id}) - {device.controllable_name}")
-        print(f"{device.widget} - {device.ui_class}")
-        # print(device.states)
-        # print(device.definition)
+        # Create an event listener and poll it
+        listener_id = await client.register_event_listener()
 
-    # Create an event listener and poll it
-    listener_id = await client.register_event_listener()
+        while True:
+            events = await client.fetch_event_listener(listener_id)
+            print(events)
 
-    while True:
-        events = await client.fetch_event_listener(listener_id)
-        print(events)
-
-        time.sleep(2)
-
-    # Make sure you call client.close() when you are done
-    await client.close()
+            time.sleep(2)
 
 
 asyncio.run(main())
