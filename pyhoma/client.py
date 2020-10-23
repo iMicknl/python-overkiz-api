8020ca35e9d33c70b5fa384ad88caeb226c199e7
vlebourl <vlebourl@gmail.com>
2020-10-23 15:57:14 +0200

    Change return type to JSON.

--- pyhoma/client.py
+++ pyhoma/client.py
@@ -129,7 +129,7 @@ class TahomaClient:
     @backoff.on_exception(
         backoff.expo, NotAuthenticatedException, max_tries=2, on_backoff=relogin
     )
-    async def get_device_definition(self, deviceurl: str) -> Dict[str, Any]:
+    async def get_device_definition(self, deviceurl: str) -> JSON:
         """
         Retrieve a particular setup device definition
         """
