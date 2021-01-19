cb9c3a6df6f5b5ae2368ed50746b6a3c257c7008
Mick Vleeshouwer <mick@imick.nl>
2021-01-19 20:01:19 +0100

    Don’t do special tricks ;-)

--- pyhoma/models.py
+++ pyhoma/models.py
@@ -366,7 +366,7 @@ class Gateway:
         *,
         partners: Optional[List[Dict[str, Any]]] = None,
         functions: Optional[str] = None,
-        sub_type: Optional[GatewaySubType] = None,
+        sub_type: GatewaySubType,
         gateway_id: str,
         alive: bool,
         mode: str,
@@ -399,4 +399,4 @@ class Gateway:
         try:
             self.sub_type = GatewaySubType(sub_type)
         except ValueError:
-            self.sub_type = None  # type: ignore
+            self.sub_type = sub_type
