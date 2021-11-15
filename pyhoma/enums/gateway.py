fa737ff689c75e28d59dbbb7819b1ccb9e124172
Mick Vleeshouwer <mick@imick.nl>
2021-11-15 02:50:43 -0800

    Add Overkiz State, Command, CommandParameter enums (#228)
    * Add enums
    
    * Extend enum
    
    * Split enums over files
    
    * Fix tests
    
    * Fix tests (2)
    
    * Style fix
    
    * Move import to .enums
    
    * Fix tests
    
    * Fix typo
    
    * Fix typo
    
    * Fix typo
    
    * Fix widget style
    
    * Fix names

---
+++ pyhoma/enums/gateway.py
@@ -0,0 +1,94 @@
+import logging
+from enum import Enum, IntEnum, unique
+
+_LOGGER = logging.getLogger(__name__)
+
+
+@unique
+class GatewayType(IntEnum):
+    UNKNOWN = -1
+    VIRTUAL_KIZBOX = 0
+    KIZBOX_V1 = 2
+    TAHOMA = 15
+    VERISURE_ALARM_SYSTEM = 20
+    KIZBOX_MINI = 21
+    HI_KUMO_ADAPTER = 22  # Hi Kumo Adapter SPX-WFG01 (constant added manually)
+    KIZBOX_V2 = 24
+    MYFOX_ALARM_SYSTEM = 25
+    KIZBOX_MINI_VMBUS = 27
+    KIZBOX_MINI_IO = 28
+    TAHOMA_V2 = 29
+    KIZBOX_V2_3H = 30
+    KIZBOX_V2_2H = 31
+    COZYTOUCH = 32
+    CONNEXOON = 34
+    JSW_CAMERA = 35
+    TAHOMA_V2_RTS = 41
+    KIZBOX_MINI_MODBUS = 42
+    KIZBOX_MINI_OVP = 43
+    HI_BOX = 44  # Hi Kumo AHP-SMB01 Hi Box (constant added manually)
+    CONNEXOON_RTS = 53
+    OPENDOORS_LOCK_SYSTEM = 54
+    CONNEXOON_RTS_JAPAN = 56
+    HOME_PROTECT_SYSTEM = 58
+    CONNEXOON_RTS_AUSTRALIA = 62
+    THERMOSTAT_SOMFY_SYSTEM = 63
+    SMARTLY_MINI_DAUGHTERBOARD_ZWAVE = 65
+    SMARTLY_MINIBOX_RAILDIN = 66
+    TAHOMA_BEE = 67
+    TAHOMA_RAIL_DIN = 72
+    NEXITY_RAIL_DIN = 74
+    ELIOT = 77
+    WISER = 88
+    TAHOMA_SWITCH = 98
+
+    @classmethod
+    def _missing_(cls, value):  # type: ignore
+        _LOGGER.warning(f"Unsupported value {value} has been returned for {cls}")
+        return cls.UNKNOWN
+
+    @property
+    def beautify_name(self) -> str:
+        return self.name.replace("_", " ").title()
+
+
+@unique
+class GatewaySubType(IntEnum):
+    UNKNOWN = -1
+    TAHOMA_BASIC = 1
+    TAHOMA_BASIC_PLUS = 2
+    TAHOMA_PREMIUM = 3
+    SOMFY_BOX = 4
+    HITACHI_BOX = 5
+    MONDIAL_BOX = 6
+    MAROC_TELECOM_BOX = 7
+    TAHOMA_SERENITY = 8
+    TAHOMA_VERISURE = 9
+    TAHOMA_SERENITY_PREMIUM = 10
+    TAHOMA_MONSIEUR_STORE = 11
+    TAHOMA_MAISON_AVENIR_ET_TRADITION = 12
+    TAHOMA_SHORT_CHANNEL = 13
+    TAHOMA_PRO = 14
+    TAHOMA_SECURITY_SHORT_CHANNEL = 15
+    TAHOMA_SECURITY_PRO = 16
+    # TAHOMA_BOX_C_IO = 12 That’s probably 17, but tahomalink.com says it’s 12
+
+    @classmethod
+    def _missing_(cls, value):  # type: ignore
+        _LOGGER.warning(f"Unsupported value {value} has been returned for {cls}")
+        return cls.UNKNOWN
+
+    @property
+    def beautify_name(self) -> str:
+        return self.name.replace("_", " ").title()
+
+
+@unique
+class UpdateBoxStatus(str, Enum):
+    NOT_UPDATABLE = "NOT_UPDATABLE"
+    READY_TO_UPDATE = "READY_TO_UPDATE"
+    READY_TO_BE_UPDATED_BY_SERVER = "READY_TO_BE_UPDATED_BY_SERVER"
+    READY_TO_UPDATE_LOCALLY = "READY_TO_UPDATE_LOCALLY"
+    UP_TO_DATE = "UP_TO_DATE"
+    UNKNOWN = "UNKNOWN"
+    UPDATING = "UPDATING"
