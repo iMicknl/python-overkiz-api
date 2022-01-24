c58f70e191084b81ad794543014d0e9583e733a3
Thibaut Etienne <thibaut@etienne.pw>
2022-01-24 14:00:01 +0100

    Add protocol property

---
+++ pyoverkiz/enums/protocol.py
@@ -0,0 +1,31 @@
+from enum import Enum, unique
+
+
+@unique
+class Protocol(str, Enum):
+    # From /reference/protocolTypes
+    IO = "io"
+    RTS = "rts"
+    RTD = "rtd"
+    RTDS = "rtds"
+    RAMSES = "ramses"
+    ENOCEAN = "enocean"
+    ZWAVE = "zwave"
+    CAMERA = "camera"
+    OVP = "ovp"
+    MODBUS = "modbus"
+    HUE = "hue"
+    VERISURE = "verisure"
+    INTERNAL = "internal"
+    JSW = "jsw"
+    OPENDOORS = "opendoors"
+    MYFOX = "myfox"
+    SOMFY_THERMOSTAT = "somfythermostat"
+    ZIGBEE = "zigbee"
+    UPNP_CONTROL = "upnpcontrol"
+    ELIOT = "eliot"
+    WISER = "wiser"
+    PROFALUX_868 = "profalux868"
+    OGP = "ogp"
+    HOMEKIT = "homekit"
+    AUGUST = "august"
