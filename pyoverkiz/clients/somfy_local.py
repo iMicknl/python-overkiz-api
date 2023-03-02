1be84ee5352ada77042a057b9666e8635e5a5f64
Thibaut Etienne <thibaut@etienne.pw>
2023-03-02 09:29:49 +0100

    Raise an error if we try to call login on the Somfy Local API

--- pyoverkiz/clients/somfy_local.py
+++ pyoverkiz/clients/somfy_local.py
@@ -10,8 +10,8 @@ class SomfyLocalClient(OverkizClient):
     _ssl: bool = field(default=False, init=False)
 
     async def _login(self, _username: str, _password: str) -> bool:
-        """There is no login for Somfy Local"""
-        return True
+        """There is no login needed for Somfy Local API"""
+        raise NotImplementedError("There is no login needed for the Somfy Local API")
 
     @property
     def _headers(self) -> dict[str, str]:
