e1a87a118b04b3f1702d8747621eda89a0b95405
Mick Vleeshouwer <mick@imick.nl>
2023-06-25 22:12:50 +0200

    Add util

--- pyoverkiz/const.py
+++ pyoverkiz/const.py
@@ -17,6 +17,12 @@ SOMFY_API = "https://accounts.somfy.com"
 SOMFY_CLIENT_ID = "0d8e920c-1478-11e7-a377-02dd59bd3041_1ewvaqmclfogo4kcsoo0c8k4kso884owg08sg8c40sk4go4ksg"
 SOMFY_CLIENT_SECRET = "12k73w1n540g8o4cokg0cw84cog840k84cwggscwg884004kgk"
 
+SERVERS_THAT_SUPPORT_LOCAL_API = [
+    Server.SOMFY_EUROPE,
+    Server.SOMFY_OCEANIA,
+    Server.SOMFY_AMERICA,
+]
+
 SUPPORTED_SERVERS: dict[str, OverkizServer] = {
     Server.ATLANTIC_COZYTOUCH: OverkizServer(
         name="Atlantic Cozytouch",
---
+++ pyoverkiz/utils.py
@@ -0,0 +1,18 @@
+from __future__ import annotations
+
+from pyoverkiz.enums import Server
+from pyoverkiz.models import OverkizServer
+
+def generate_local_server(
+    host: str,
+    name: str = Server.SOMFY_DEVELOPER_MODE,
+    manufacturer: str = "Somfy",
+    configuration_url: str | None = None,
+) -> OverkizServer:
+    """Generate OverkizServer class for connection with a local API (Somfy Developer mode)."""
+    return OverkizServer(
+        name=name,
+        endpoint=f"https://{host}/enduser-mobile-web/1/enduserAPI/",
+        manufacturer=manufacturer,
+        configuration_url=configuration_url,
+    )
