28600f7b368cb160d1a49b8f2e554f8a3f4da6ff
Mick Vleeshouwer <mick@imick.nl>
2023-06-26 07:35:09 +0000

    Extend tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,11 +1,11 @@
 from pyoverkiz.utils import generate_local_server
 
 LOCAL_HOST = "gateway-1234-5678-1243.local:8443"
+LOCAL_HOST_BY_IP = "192.168.1.105:8443"
 
 
 class TestUtils:
-    def test_empty_states(self):
-
+    def test_generate_local_server(self):
         local_server = generate_local_server(host=LOCAL_HOST)
 
         assert local_server
@@ -15,3 +15,16 @@ class TestUtils:
         )
         assert local_server.manufacturer == "Somfy"
         assert local_server.name == "Somfy Developer Mode"
+
+    def test_generate_local_server_by_ip(self):
+        local_server = generate_local_server(
+            host=LOCAL_HOST_BY_IP, manufacturer="Test Manufacturer", name="Test Name"
+        )
+
+        assert local_server
+        assert (
+            local_server.endpoint
+            == "https://192.168.1.105:8443/enduser-mobile-web/1/enduserAPI/"
+        )
+        assert local_server.manufacturer == "Test Manufacturer"
+        assert local_server.name == "Test Name"
