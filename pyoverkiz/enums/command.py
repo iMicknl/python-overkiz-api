30afbe26bb8d20538e848d84cb0dacf6bd80f60c
Mick Vleeshouwer <mick@imick.nl>
2023-08-03 20:45:21 +0200

    Add StrEnum

--- pyoverkiz/enums/command.py
+++ pyoverkiz/enums/command.py
@@ -1,8 +1,14 @@
-from enum import Enum, unique
+from enum import unique
+import sys
+
+if sys.version_info >= (3, 11):
+    from enum import StrEnum
+else:
+    from backports.strenum import StrEnum
 
 
 @unique
-class OverkizCommand(str, Enum):
+class OverkizCommand(StrEnum):
     """Device commands used by Overkiz."""
 
     ACTIVATE_OPTION = "activateOption"
@@ -131,7 +137,7 @@ class OverkizCommand(str, Enum):
 
 
 @unique
-class OverkizCommandParam(str, Enum):
+class OverkizCommandParam(StrEnum):
     """Parameter used by Overkiz commands and/or states."""
 
     A = "A"
@@ -259,7 +265,7 @@ class OverkizCommandParam(str, Enum):
 
 
 @unique
-class CommandMode(str, Enum):
+class CommandMode(StrEnum):
     HIGH_PRIORITY = "highPriority"
     GEOLOCATED = "geolocated"
     INTERNAL = "internal"
