20cc921ccea83b620e35c0ee7e7fcfa003027d30
Johnny Willemsen <jwillemsen@remedy.nl>
2024-02-29 15:50:14 +0100

    Test obfuscate_sensitive_data with a list which has some None values
        * tests/test_obfuscate.py:

--- tests/test_obfuscate.py
+++ tests/test_obfuscate.py
@@ -1,6 +1,6 @@
 import pytest
 
-from pyoverkiz.obfuscate import obfuscate_email
+from pyoverkiz.obfuscate import obfuscate_email, obfuscate_sensitive_data
 
 LOCAL_HOST = "gateway-1234-5678-1243.local:8443"
 LOCAL_HOST_BY_IP = "192.168.1.105:8443"
@@ -16,3 +16,8 @@ class TestObfucscate:
     )
     def test_email_obfuscate(self, email: str, obfuscated: str):
         assert obfuscate_email(email) == obfuscated
+
+class TestObfucscateSensitive:
+    def test_obfuscate_list_with_none(self):
+        input = {'d': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, None, None, None, None, None, None, None]}
+        assert obfuscate_sensitive_data(input) == input
