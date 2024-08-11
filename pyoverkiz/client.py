4f18603647de6985769f76b74c406a943ed98cd9
Mick Vleeshouwer <mick@imick.nl>
2024-08-11 18:03:30 +0000

    Fix

--- pyoverkiz/client.py
+++ pyoverkiz/client.py
@@ -954,8 +954,7 @@ class OverkizClient:
                 raise ServiceUnavailableException(result) from error
 
             raise OverkizException(
-                f"Unknown error while requesting {
-                    response.url}. {response.status} - {result}"
+                f"Unknown error while requesting {response.url}. {response.status} - {result}"
             ) from error
 
         if result.get("errorCode"):
